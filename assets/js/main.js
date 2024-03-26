1feff182593fdd17350d7241d89ca35498cdc0c7
unknown <gouravbishnoi429@gmail.com>
2024-03-26 12:17:09 +0530

    back to top button added

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,22 @@ function openNav() {
 //       }, 3000);
 //     });
 //   });
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
         $('.responsive_1').slick({
             dots: true,
