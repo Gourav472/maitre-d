ca062d439dae88e74121c94da729bf3c24de7cf3
unknown <gouravbishnoi429@gmail.com>
2024-03-29 15:33:26 +0530

    errors solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,87 +1,87 @@
 function openNav() {
-    document.getElementById("navbar").classList.toggle("start-0");
-    document.body.classList.toggle("overflow_hidden");
-    document.querySelector(".menu").classList.toggle("cross")
-  }
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    window.addEventListener("load", function () {
-      setTimeout(function () {
-        let loadingOverlay = document.getElementById("loadingOverlay");
-        loadingOverlay.style.display = "none";
-        document.querySelector("body").classList.remove("overflow_hidden");
-      }, 3000);
-    });
+  document.getElementById("navbar").classList.toggle("start-0");
+  document.body.classList.toggle("overflow_hidden");
+  document.querySelector(".menu").classList.toggle("cross")
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      let loadingOverlay = document.getElementById("loadingOverlay");
+      loadingOverlay.style.display = "none";
+      document.querySelector("body").classList.remove("overflow_hidden");
+    }, 3000);
   });
-let mybutton = document.getElementById("myBtn");
+});
+const mybutton = document.getElementById("myBtn");
 
 window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
-}
+};
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
-        $('.responsive_1').slick({
-            dots: false,
-            infinite: true,
-            speed: 500,
-            autoplay: false,
-            arrows: true,
-            prevArrow: ".prev_arrow_1",
-            nextArrow: ".next_arrow_1",
-            slidesToShow: 1,
-            slidesToScroll: 1,
+$('.responsive_1').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  autoplay: false,
+  arrows: true,
+  prevArrow: ".prev_arrow_1",
+  nextArrow: ".next_arrow_1",
+  slidesToShow: 1,
+  slidesToScroll: 1,
 
-            responsive: [
-                {
-                    breakpoint: 1440,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
 
-                    }
-                },
-                {
-                    breakpoint: 1200,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    }
-                },
-                {
-                    breakpoint: 992,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                    }
-                },
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        arrows: false,
-                        autoplay: true,
-                    }
-                },
-                {
-                    breakpoint: 576,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        arrows: false,
-                        autoplay: true,
-                    }
-                }
+      }
+    },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+      }
+    },
+    {
+      breakpoint: 576,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+      }
+    }
 
-            ]
-        });
+  ]
+});
